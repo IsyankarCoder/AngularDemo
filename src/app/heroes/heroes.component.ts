6bc6f3d80d0c6f9e89ef6a318282125a7ab6a1ea
isyankarcoder <gencvolkan@gmail.com>
2020-04-25 00:45:15 +0300

    liste deneme

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import { HeroList } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,15 @@ import {Hero} from '../hero';
 export class HeroesComponent implements OnInit {
   /*hero:string= 'Volkan Tolkan';*/
   myHero:Hero;
+  heroList:Hero[];
   constructor() { 
   
     this.myHero={
       id:1,
       name:'Volkan'
     }
+
+    this.heroList= HeroList;
   }
 
   ngOnInit(): void {
